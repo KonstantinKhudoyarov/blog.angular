7813fd1e5eff50cc598816f9b38d0d7f26ff53e1
KonstantinKhudoyarov <kostya040390@gmail.com>
2019-11-26 10:59:12 +0200

    Add functionality for save and clear token in to local storage

--- src/app/admin/shared/sevices/auth.service.ts
+++ src/app/admin/shared/sevices/auth.service.ts
@@ -13,7 +13,12 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   get token(): string {
-    return '';
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
@@ -23,13 +28,21 @@ export class AuthService {
       );
   }
 
-  logout(){}
+  logout() {
+    this.setToken(null);
+  }
 
   isAuthenticated(): boolean {
     return !!this.token;
   }
 
-  private setToken(response: FBAuthResponse) {
-    console.log(response);
+  private setToken(response: FBAuthResponse | null) {
+    if (response) {
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
   }
 }
